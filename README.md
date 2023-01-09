29f48266bc18796aa56735299c5fc66059354740
nicholaslu <nicholaslu1996@gmail.com>
2023-01-09 16:15:45 +0900

    Image test

--- README.md
+++ README.md
@@ -2,12 +2,12 @@
 Add ROS-based camera image transfer support to ESP32.
 
 ## Hardware
-ESP32-CAM
+[ESP32-CAM](https://docs.platformio.org/en/latest/boards/espressif32/esp32cam.html)
 
 ## Environment
 PlatformIO on VS Code
 
-# Network setting
+## Network setting
 Under `\src`, add new file `secret.c` with the content below:
 ```
 const char* ssid     = "ssid";
@@ -21,4 +21,14 @@ Modify IP address and server port in `main.cpp`.
 Under `\lib`, run: 
 ```
 git clone -b noetic https://github.com/nicholaslu/ros_lib.git
+```
+Modify `\lib\ros_lib\ros\node_handle.h` to increase buffer size:
+
+```
+/* Node Handle */
+template<class Hardware,
+         int MAX_SUBSCRIBERS = 25,
+         int MAX_PUBLISHERS = 25,
+         int INPUT_SIZE = 1024,
+         int OUTPUT_SIZE = 65536>
 ```
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,12 @@
 #include <Arduino.h>
 #include "esp_camera.h"
 #include <WiFi.h>
+// #include "dl_lib_matrix3d.h"
 
 #define ROSSERIAL_ARDUINO_TCP
 
 #include <ros.h>
-#include <sensor_msgs/CompressedImage.h>
+#include <sensor_msgs/Image.h>
 //
 // WARNING!!! PSRAM IC required for UXGA resolution and high JPEG quality
 //            Ensure ESP32 Wrover Module or other board with PSRAM is selected
@@ -33,11 +34,18 @@ const uint16_t serverPort = 11411;
 
 ros::NodeHandle nh;
 // Make a chatter publisher
-sensor_msgs::CompressedImage cimg_msg;
-ros::Publisher cimg_pub("esp32_cam", &cimg_msg);
+sensor_msgs::Image img_msg;
+ros::Publisher img_pub("esp32_cam", &img_msg);
 
-char frame_id[] = "/esp32_camera";
-char format[] = "jpeg";
+char frame_id[] = "esp32_camera";
+// uint32_t height = 640;
+// uint32_t width = 480;
+char encoding[] = "rgb8";
+uint8_t is_bigendian = true;
+// uint32_t step = 1920;
+// uint8_t data[1920*480];
+uint32_t array_size = 96*96*3;
+// uint32_t array_size = 512;
 
 // void startCameraServer();
 
@@ -70,15 +78,15 @@ void setup() {
   
   // if PSRAM IC present, init with UXGA resolution and higher JPEG quality
   //                      for larger pre-allocated frame buffer.
-  if(psramFound()){
-    config.frame_size = FRAMESIZE_UXGA;
-    config.jpeg_quality = 10;
-    config.fb_count = 2;
-  } else {
-    config.frame_size = FRAMESIZE_SVGA;
-    config.jpeg_quality = 12;
-    config.fb_count = 1;
-  }
+  // if(psramFound()){
+  //   config.frame_size = FRAMESIZE_UXGA;
+  //   config.jpeg_quality = 80;
+  //   config.fb_count = 2;
+  // } else {
+  config.frame_size = FRAMESIZE_96X96;
+  config.jpeg_quality = 64;
+  config.fb_count = 1;
+  // }
 
 #if defined(CAMERA_MODEL_ESP_EYE)
   pinMode(13, INPUT_PULLUP);
@@ -100,7 +108,7 @@ void setup() {
     s->set_saturation(s, -2); // lower the saturation
   }
   // drop down frame size for higher initial frame rate
-  s->set_framesize(s, FRAMESIZE_QVGA);
+  s->set_framesize(s, FRAMESIZE_96X96);
 
 #if defined(CAMERA_MODEL_M5STACK_WIDE) || defined(CAMERA_MODEL_M5STACK_ESP32CAM)
   s->set_vflip(s, 1);
@@ -126,41 +134,90 @@ void setup() {
   nh.initNode();
 
   // Start to be polite
-  nh.advertise(cimg_pub);
+  nh.advertise(img_pub);
+
+  Serial.print("Heap before malloc: ");
+  Serial.println(ESP.getFreeHeap());
+
+  img_msg.data = (uint8_t *) malloc(sizeof(uint8_t)*array_size);
+  img_msg.data_length = array_size;
+
+  Serial.print("Heap after malloc: ");
+  Serial.println(ESP.getFreeHeap());
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   if (nh.connected()) {
     Serial.println("Connected");
-    // Say hello
-    cimg_msg.header.frame_id = frame_id;
-    cimg_msg.header.stamp = nh.now();
-    cimg_msg.format = format;
-    // uint8_t jpeg_data[] = {255,255,255};
-    // cimg_msg.data = jpeg_data;
 
     camera_fb_t * fb = NULL;
     fb = esp_camera_fb_get();
-    size_t _jpg_buf_len;
-    uint8_t * _jpg_buf;
-    bool jpeg_converted = frame2jpg(fb, 80, &_jpg_buf, &_jpg_buf_len);
-    if (jpeg_converted){
-      // cimg_msg.data = _jpg_buf;
-      // cimg_msg.data = uint8_t (*_jpg_buf)[_jpg_buf_len];
-      // uint8_t jpeg_data[_jpg_buf_len];
-      uint8_t (*ptr)[_jpg_buf_len];
-      ptr = (uint8_t(*)[_jpg_buf_len])_jpg_buf;
-      // cimg_msg.data = *_jpg_buf;
-      cimg_msg.data = *ptr;
-      int i;
+    if (!fb){
+      Serial.println("Failed to capture");
+      delay(1000);
+      return;
+    }
+
+    img_msg.header.frame_id = frame_id;
+    img_msg.header.stamp = nh.now();
+    // img_msg.height = fb->height;
+    img_msg.height = 96;
+    // img_msg.width = fb->width;
+    img_msg.width = 96;
+    img_msg.encoding = encoding;
+    img_msg.is_bigendian = is_bigendian;
+    // img_msg.step = img_msg.width*3;
+    img_msg.step = 96*3;
+
+    // size_t size = img_msg.height*img_msg.width*3;
+    // dl_matrix3du_t *image_matrix = dl_matrix3du_alloc(1, img_msg.width, img_msg.height, 3);
+    // uint8_t *out_buf = image_matrix->item;
+
+    // uint8_t * data;
+    // Serial.println(sizeof(uint8_t)*size);
+    // img_msg.data = (uint8_t *) malloc(size);
+    // img_msg.data_length = size;
+    // data = (uint8_t *) malloc(size);
+    bool cov = fmt2rgb888(fb->buf, array_size, fb->format, img_msg.data);
+    // for (int i=0; i<array_size; i++){
+    //   img_msg.data[i] = i%256;
+    // }
+    esp_camera_fb_return(fb);
+    // bool cov = fmt2rgb888(fb->buf, size, fb->format, out_buf);
+    if(!cov){
+      Serial.println("Failed to convert");
+      delay(1000);
+      return;
+    }
+    Serial.println("Message ready to send");
+    img_pub.publish(&img_msg);
+    Serial.println("Message sent");
+    // free(img_msg.data);
+    
+    // img_msg.data = data;
+
+    // dl_matrix3du_free(image_matrix);
+    // free(data);
+    // size_t _jpg_buf_len;
+    // uint8_t * _jpg_buf;
+    // bool jpeg_converted = fmt2rgb888(fb, 80, &_jpg_buf, &_jpg_buf_len);
+    // if (jpeg_converted){
+    //   // cimg_msg.data = _jpg_buf;
+    //   // cimg_msg.data = uint8_t (*_jpg_buf)[_jpg_buf_len];
+    //   // uint8_t jpeg_data[_jpg_buf_len];
+    //   uint8_t (*ptr)[_jpg_buf_len];
+    //   ptr = (uint8_t(*)[_jpg_buf_len])_jpg_buf;
+    //   // cimg_msg.data = *_jpg_buf;
+    //   img_msg.data = *ptr;
+    //   int i;
       // for (i = 0; i < 100; i++){
       //   Serial.println(cimg_msg.data[i]);
       // }
-    }
-    cimg_pub.publish( &cimg_msg );
-    esp_camera_fb_return(fb);
-    free(_jpg_buf);
+    // }
+    // img_pub.publish( &img_msg );
+    // esp_camera_fb_return(fb);
+    // free(_jpg_buf);
   } else {
     Serial.println("Not Connected");
   }
